9038083339701765091969a212776d85a088558d
Lucas-Santos <lucassantos2018d@gmail.com>
2023-12-17 17:49:36 -0300

    Classificador de heróis

--- Projeto-Dio.py
+++ Projeto-Dio.py
@@ -1,1 +1,23 @@
-xp = int(input('Digite o valor da experiência do herói:'))
+print(20*'+=')
+print('Classificador de nível de Herói')
+print(20*'+=')
+
+nome = str(print('Digite o nome do héroi:'))
+xp = int(input('Digite a experiência do herói: (XP)'))
+if xp < 1000:git
+    print('Classificação: Ferro')
+elif xp >= 1001 and xp <= 2000:
+    print('Classificação: Bronze')
+elif xp >= 2001 and xp <= 5000:
+    print('Classificação: Prata')
+elif xp >= 6001 and xp <= 7000:
+    print('Classificação: Ouro')
+elif xp >= 7001 and xp <= 8000:
+    print('Classificação: Platina')
+elif xp >= 8001 and xp <= 9000:
+    print('Classificação: ascendente')
+elif xp >= 9001 and xp <= 10000:
+    print('Classificação: Imortal')
+else:
+    print('Classificação: Radiante, parabéns!')
+
